b2ab9e53e5c63a0474f99a8256a29fb2414ef54e
kallyane-nascimento16 <110573013+kallyane-nascimento16@users.noreply.github.com>
2022-09-28 18:41:08 -0300

    Add files via upload

---
+++ cadastro_novoo/cadastro/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace cadastro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelnome = new System.Windows.Forms.Label();
+            this.labelCPF = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttoncadastro = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelnome
+            // 
+            this.labelnome.AutoSize = true;
+            this.labelnome.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelnome.ForeColor = System.Drawing.Color.Transparent;
+            this.labelnome.Location = new System.Drawing.Point(22, 49);
+            this.labelnome.Name = "labelnome";
+            this.labelnome.Size = new System.Drawing.Size(71, 25);
+            this.labelnome.TabIndex = 0;
+            this.labelnome.Text = "NOME";
+            // 
+            // labelCPF
+            // 
+            this.labelCPF.AutoSize = true;
+            this.labelCPF.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCPF.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelCPF.Location = new System.Drawing.Point(22, 183);
+            this.labelCPF.Name = "labelCPF";
+            this.labelCPF.Size = new System.Drawing.Size(47, 25);
+            this.labelCPF.TabIndex = 1;
+            this.labelCPF.Text = "CPF";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Unicode MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(22, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ENDEREÇO";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(211, 189);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(332, 25);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(211, 110);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(332, 25);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.textBox3.Location = new System.Drawing.Point(211, 49);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(332, 25);
+            this.textBox3.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(27, 269);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(437, 160);
+            this.listBox1.TabIndex = 6;
+            // 
+            // buttoncadastro
+            // 
+            this.buttoncadastro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttoncadastro.Location = new System.Drawing.Point(572, 311);
+            this.buttoncadastro.Name = "buttoncadastro";
+            this.buttoncadastro.Size = new System.Drawing.Size(139, 70);
+            this.buttoncadastro.TabIndex = 7;
+            this.buttoncadastro.Text = "CADASTRO";
+            this.buttoncadastro.UseVisualStyleBackColor = true;
+            this.buttoncadastro.Click += new System.EventHandler(this.buttoncadastro_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttoncadastro);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelCPF);
+            this.Controls.Add(this.labelnome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelnome;
+        private System.Windows.Forms.Label labelCPF;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttoncadastro;
+    }
+}
+
---
+++ cadastro_novoo/cadastro/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cadastro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttoncadastro_Click(object sender, EventArgs e)
+        {
+            pessoa obs=new pessoa();
+            obs.nome=textBox3.Text;
+            obs.endereco=textBox2.Text;
+            obs.cpf=int.Parse(textBox1.Text);
+            
+            listBox1.Items.Add(obs);
+            listBox1.Items.Add("DADOS :");
+            listBox1.Items.Add("nome do usuario :" +obs.nome); 
+            listBox1.Items.Add("o endereço do usuario :" +obs.endereco);
+            listBox1.Items.Add("o CPF do usuario :" + obs.cpf);
+
+        }
+    }
+}
